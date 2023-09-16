d16724f791073abd517640d211161220f2131edd
fqathf <100032449+fqathf@users.noreply.github.com>
2023-09-16 15:51:45 +0700

    Update

--- pages/fintrack.vue
+++ pages/fintrack.vue
@@ -3,66 +3,74 @@
     <br />
     <div class="d-flex justify-content-between">
       <section class="form">
-        <form v-on:submit.prevent="handleSubmit">
+        <b-form v-on:submit.prevent="handleSubmit">
           <div class="field">
-            <label class="label">Deskripsi</label>
-            <div class="control">
-              <textarea
-                v-model="form.desc"
-                class="textarea"
-                placeholder="Isi Deskripsi Transaksi Anda di Sini"
-                required
-              ></textarea>
-            </div>
             <label class="label">Jumlah/Nominal</label>
             <div class="control">
               <input
-                v-model="form.amount"
-                class="input"
+                v-model="form.nominal"
+                class="form-control input"
                 type="number"
                 placeholder="Isi Nominal Transaksi"
                 required
               />
             </div>
-            <label class="label">Jenis Transaksi</label>
             <div class="control">
-              <label class="radio">
-                <input
-                  v-model="form.tc_type"
-                  type="radio"
+              <b-form-group
+                label="Jenis Transaksi"
+                v-slot="{ ariaDescribedby }"
+              >
+                <b-form-radio
+                  v-model="form.jenis_transaksi"
+                  :aria-describedby="ariaDescribedby"
+                  name="some-radios"
                   value="Debit"
                   required
-                />Debit
-              </label>
-              <label class="radio">
-                <input
-                  v-model="form.tc_type"
-                  type="radio"
+                  >Debit</b-form-radio
+                >
+                <b-form-radio
+                  v-model="form.jenis_transaksi"
+                  :aria-describedby="ariaDescribedby"
+                  name="some-radios"
                   value="Kredit"
                   required
-                />Kredit
-              </label>
+                  >Kredit</b-form-radio
+                >
+              </b-form-group>
             </div>
             <label class="label">Waktu</label>
             <div class="control">
-              <input v-model="form.date" type="date" name="" id="" required />
+              <b-form-datepicker
+                id=""
+                v-model="form.tanggal"
+                class="mb-2"
+              ></b-form-datepicker>
             </div>
-            <br />
+            <label class="label">Deskripsi Transaksi</label>
             <div class="control">
-              <button class="button is-primary">Submit</button>
+              <b-form-textarea
+                id="textarea"
+                v-model="form.deskripsi"
+                placeholder="Isi Deskripsi Transaksi Anda di Sini..."
+                rows="3"
+                max-rows="6"
+              ></b-form-textarea>
+            </div> <br>
+            <div class="control">
+              <button class="btn btn-primary">Submit</button>
             </div>
             <br />
           </div>
-        </form>
+        </b-form>
       </section>
       <div>
-        <h3>Hasil Submit Form</h3>
+        <h3>Riwayat Transaksi</h3>
         <!--ul>
           <li v-for="item in inputanForm" :key="item">
             {{ form }}
           </li>
         </ul-->
-        <table class="table">
+        <!--table class="table">
           <thead class="thead-light">
             <tr>
               <th scope="col">Deskripsi</th>
@@ -72,7 +80,12 @@
             </tr>
           </thead>
           <tbody>
-            <!--CardItem /-->
+            <CardItem
+              v-for="(forms, i) in resultQuery"
+              :key="i"
+              :forms="forms"
+              :isGrid="isGrid"
+            />
             <tr>
               <td v-for="item in inputanForm" :key="item">
                 {{ form.desc }}
@@ -88,58 +101,60 @@
               </td>
             </tr>
           </tbody>
-        </table>
-        <h5 v-for="item in inputanForm" :key="item">Total: {{ form.total }}</h5>
+        <table-->
+        <b-table striped hover :items="forms"></b-table>
+        <h5>Total: {{ form.total }}</h5>
       </div>
     </div>
   </div>
 </template>
 <script>
-/*import CardItem from '@/components/Card/CardItem.vue'*/
+import CardItem from '@/components/Card/CardItem.vue'
 
 export default {
   layout(context) {
     return 'custom'
   },
+  components: {
+    CardItem,
+  },
   data() {
     return {
       form: {
-        amount: undefined,
-        desc: '',
-        tc_type: '',
-        date: '',
-        total: 0,
+        nominal: undefined,
+        jenis_transaksi: '',
+        tanggal: '',
+        deskripsi: '',
+        total: 500000
       },
-      inputanForm: [],
-      /*forms: [
+      forms: [
         {
-          amount: 500000,
-          desc: 'Tabungan',
-          tc_type: 'Debit',
-          date: '2023-09-16',
-          total: 500000,
+          nominal: 500000,
+          jenis_transaksi: 'Debit',
+          tanggal: '2023-09-16',
+          deskripsi: 'Tabungan',
         },
-      ],*/
+      ],
     }
   },
   methods: {
     handleSubmit() {
-      // console.log(this.form)
+      console.log(this.form)
       const item = {
         form: this.form,
       }
-      if (item.form.tc_type == 'Debit') {
-        item.form.total += parseInt(item.form.amount)
-      } else if (item.form.tc_type == 'Kredit') {
-        item.form.total -= item.form.amount
+      if (item.form.jenis_transaksi == 'Debit') {
+        item.form.total += parseInt(item.form.nominal)
+      } else if (item.form.jenis_transaksi == 'Kredit') {
+        item.form.total -= parseInt(item.form.nominal)
       }
-      this.inputanForm.push(item)
+      this.forms.push(this.form)
     },
   },
   /*computed: {
     resultQuery() {
       if (this.searchQuery) {
-        return this.form.filter((item) => {
+        return this.formT.filter((item) => {
           return this.searchQuery.toLowerCase
             .split(' ')
             .every((v) => item.title.toLowerCase().include(v))
